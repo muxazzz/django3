4b34257f5934e422376d5362f6ebb205a4a60b69
Muxazzz <pu4kov.mikhail@yandex.ru>
2020-06-11 17:01:21 +0300

    initial commit

---
+++ p_library/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+class Author(models.Model):  
+    full_name = models.TextField()  
+    birth_year = models.SmallIntegerField()  
+    country = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.full_name
+
+class Publisher(models.Model):  
+    title = models.TextField()  
+    def __str__(self):
+        return self.title
+
+class Friend(models.Model):  
+    f_name = models.TextField()  
+    def __str__(self):
+        return self.f_name
+
+
+class Book(models.Model):  
+    ISBN = models.CharField(max_length=13)  
+    title = models.TextField()  
+    description = models.TextField()  
+    year_release = models.SmallIntegerField()  
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    price = models.DecimalField(default=None, max_digits=6, decimal_places=2)
+    copy_count = models.SmallIntegerField(default=1)
+    publisher = models.ForeignKey(Publisher, blank=True, null=True, on_delete=models.CASCADE)
+    friend = models.ForeignKey(Friend, blank=True, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
