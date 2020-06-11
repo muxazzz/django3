a5b1ba3ef50ddbab6ee6ba21b794ca9e0a37ad4a
Muxazzz <pu4kov.mikhail@yandex.ru>
2020-06-11 17:20:54 +0300

    about to deploy322

--- my_site/settings.py
+++ my_site/settings.py
@@ -74,12 +74,7 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
 
 
 # Password validation
